526b2dc63aa2bd0515316e6d0a4f2365bf4b8419
fedpanoz <federicopanozzo@gmail.com>
2022-01-29 05:18:55 +0100

    28.01

---
+++ for_dimmerda.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+z = int(input('Tell the end till summing:\n'))
+total = 0
+for n in range(1, (z + 1)):
+    total += n
+    print(total)
